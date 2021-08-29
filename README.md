7ab3a1a7b9092be474f4c11eb48a93663d60a2b0
‘Ankush123456-code’ <ankushkumar1840@gmail.com>
2021-08-29 13:43:38 +0530

    updated

--- README.md
+++ README.md
@@ -3,8 +3,17 @@
 
 This python package is created by Ankush kunwar
 
+Dependencies
+```
+pip install spacy
+python -m spacy download en_core_web_sm
+pip install beautifulsoup4
+pip install textblob
+```
+
+
 Install
-`pip install git+https://github.com/Ankush123456-code/preprocess_Ak.git`
+`pip install git+https://github.com/Ankush123456-code/preprocess_Ak.git --upgrade --force-reinstall`
 
 Uninstall
 `pip uninstall preprocess_Ak`
--- setup.py
+++ setup.py
@@ -5,15 +5,19 @@ with open('README.md', 'r') as file:
 
 setuptools.setup(
     name='preprocess_Ak',  ## this should be unique
+    include_package_data=True,
     version='0.0.1',
     author="Ankush kunwar",
     author_email="ankushkunwar1840@gmail.com",
+    keywords="preprocessing package",
     description='clean your text',
-    Long_despription=long_description,
-    Long_description_content="text/markdown",
-    pakage=setuptools.find_packages(),
-    Classifiers=['Programming Language:: Python :: 3',
-                 'License :: OSI Approved :: MIT License',
-                 'Operating System :: OS Independent'],
-    python_require='>=3.5'
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        "Operating System :: OS Independent"],
+    python_require='>=3.5',
+    install_requires=['requests']
 )
